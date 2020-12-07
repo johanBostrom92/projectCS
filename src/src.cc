bede5c22d60a4cc16c59dba38345b61a8dfa8ae9
Hampus Adolfsson <hampus@adolfsson.email>
2020-12-07 11:48:00 +0100

    Add uniform and low density vaccination strategies

--- src/src.cc
+++ src/src.cc
@@ -15,7 +15,7 @@
 #include <algorithm>
 #include <cassert>
 
-#define PLOT true
+#define PLOT false
 
 
 
@@ -280,7 +280,7 @@ void step(board& previous, board& current, std::mt19937_64& gen, int t) {
                     continue;
                 }
 
-                if (ONLY_ELIGIBLE) {
+                if constexpr (ONLY_ELIGIBLE) {
                     std::vector<std::tuple<int, int>> checked;
                     for (int y_box = -rad; y_box <= rad; y_box++) {
                         for (int x_box = -rad; x_box <= rad; x_box++) {
@@ -357,57 +357,71 @@ void step(board& previous, board& current, std::mt19937_64& gen, int t) {
     previous = current;
 }
 
-#define STATUS_TO_WEIGHT(s) ((s) == I ? 1 : 0)
+#define IS_INFECTED(s) ((s) == I ? 1 : 0)
 
 /**
  * Updates all vaccination weights for a board, to determine who should be more
- * or less likely to receive a vaccine. This is based on the number of infected agents within
- * INFECTION_RADIUS of each agent.
- * TODO: make it possible to customize the strategy for how the weights are chosen
+ * or less likely to receive a vaccine. This is based on the vaccination strategy
+ * denoted by the VACC_STRAT parameter.
  */
 void update_vaccination_weights(board& b) {
     b.vaccination_weight_sum = 0;
     std::vector<unsigned int> tmp(b.vaccination_weights.size());
 
-    // Essentially, this is a box blur, which we can separate into one pass for each axis
-    // (we first blur on the x axis and store it to tmp, then blur tmp on the y axis and store it back to the board)
-
-    #pragma omp parallel for
-    for (int y = 0; y < b.dim; y++) {
-        unsigned int sum = 0;
-        for (int x_box = 0; x_box < INFECTION_RADIUS; x_box++) {
-            sum += STATUS_TO_WEIGHT(b.agents[y*b.dim + x_box].status);
+    if constexpr (VACC_STRAT == vaccination_strategy::UNIFORM) {
+        std::fill(b.vaccination_weights.begin(), b.vaccination_weights.end(), 1);
+        b.vaccination_weight_sum = b.vaccination_weights.size();
+    } else if constexpr (VACC_STRAT == vaccination_strategy::HIGH_DENSITY || VACC_STRAT == vaccination_strategy::LOW_DENSITY) {
+        // Essentially, this is a box blur, which we can separate into one pass for each axis
+        // (we first blur on the x axis and store it to tmp, then blur tmp on the y axis and store it back to the board)
+        #pragma omp parallel for
+        for (int y = 0; y < b.dim; y++) {
+            unsigned int sum = 0;
+            for (int x_box = 0; x_box < INFECTION_RADIUS; x_box++) {
+                sum += IS_INFECTED(b.agents[y*b.dim + x_box].status);
+            }
+            for (int x = 0; x < b.dim; x++) {
+                if (x + INFECTION_RADIUS < b.dim) {
+                    sum += IS_INFECTED(b.agents[y*b.dim + x + INFECTION_RADIUS].status);
+                }
+                tmp[y*b.dim + x] = sum;
+                if (x - INFECTION_RADIUS >= 0) {
+                    sum -= IS_INFECTED(b.agents[int(y*b.dim) + x - INFECTION_RADIUS].status);
+                }
+            }
         }
+        unsigned int maxSum;
         for (int x = 0; x < b.dim; x++) {
-            if (x + INFECTION_RADIUS < b.dim) {
-                sum += STATUS_TO_WEIGHT(b.agents[y*b.dim + x + INFECTION_RADIUS].status);
+            unsigned int sum = 0;
+            for (int y_box = 0; y_box < INFECTION_RADIUS; y_box++) {
+                sum += tmp[y_box*b.dim + x];
             }
-            tmp[y*b.dim + x] = sum;
-            if (x - INFECTION_RADIUS >= 0) {
-                sum -= STATUS_TO_WEIGHT(b.agents[int(y*b.dim) + x - INFECTION_RADIUS].status);
+            for (int y = 0; y < b.dim; y++) {
+                if (y + INFECTION_RADIUS < b.dim) {
+                    sum += tmp[(y + INFECTION_RADIUS)*b.dim + x];
+                }
+                int idx = y*b.dim + x;
+                maxSum = std::max(sum, maxSum);
+                b.vaccination_weights[idx] = sum;
+                if (y - INFECTION_RADIUS >= 0) {
+                    sum -= tmp[(y - INFECTION_RADIUS)*int(b.dim) + x];
+                }
             }
+        }
+        for (int i = 0; i < b.agents.size(); i++) {
+            if constexpr (VACC_STRAT == vaccination_strategy::HIGH_DENSITY) {
+                b.vaccination_weights[i] += 1;
+            } else {
+                b.vaccination_weights[i] = 1 + maxSum - b.vaccination_weights[i];
+            }
+            b.vaccination_weight_sum.fetch_add(b.vaccination_weights[i], std::memory_order_acq_rel);
         }
     }
-    #pragma omp parallel for
-    for (int x = 0; x < b.dim; x++) {
-        unsigned int sum = 0;
-        for (int y_box = 0; y_box < INFECTION_RADIUS; y_box++) {
-            sum += tmp[y_box*b.dim + x];
-        }
-        for (int y = 0; y < b.dim; y++) {
-            if (y + INFECTION_RADIUS < b.dim) {
-                sum += tmp[(y + INFECTION_RADIUS)*b.dim + x];
-            }
-            int idx = y*b.dim + x;
-            int weight = 1 + sum;
-            if (b.agents[idx].status == V || b.agents[idx].vaccination_progress) {
-                weight = 0;
-            }
-            b.vaccination_weights[idx] = weight;
-            b.vaccination_weight_sum.fetch_add(weight, std::memory_order_acq_rel);
-            if (y - INFECTION_RADIUS >= 0) {
-                sum -= tmp[(y - INFECTION_RADIUS)*int(b.dim) + x];
-            }
+    // Regardless of vaccination strategy, perform a final pass to remove weight from those already vaccinated
+    for (int i = 0; i < b.agents.size(); i++) {
+        if (b.agents[i].status == V || b.agents[i].vaccination_progress || b.agents[i].status == R) {
+            b.vaccination_weight_sum -= b.vaccination_weights[i];
+            b.vaccination_weights[i] = 0;
         }
     }
 }
@@ -426,7 +440,7 @@ void vaccinate(board& b, unsigned int n_vaccinations, std::mt19937_64& gen) {
 
     // For each agent, compute the combined weight of it and all agents with a lower index
     // This lets us later use lower_bound to do a binary search for the agent to vaccinate
-    std::vector<unsigned int> cumulative_weights(b.agents.size());
+    std::vector<uint64_t> cumulative_weights(b.agents.size());
     unsigned int weight_sum = 0;
     for(int i = 0; i < b.agents.size(); i++) {
         weight_sum += b.vaccination_weights[i];
@@ -434,13 +448,13 @@ void vaccinate(board& b, unsigned int n_vaccinations, std::mt19937_64& gen) {
     }
     assert(cumulative_weights[b.agents.size() - 1] = b.vaccination_weight_sum);
 
-    std::uniform_int_distribution<int> dis(1, (int) b.vaccination_weight_sum); // Generates (cumulative) weight values determining who gets vaccinated
+    std::uniform_int_distribution<unsigned int> dis(1, (int) b.vaccination_weight_sum); // Generates (cumulative) weight values determining who gets vaccinated
     std::vector<std::atomic_flag> vaccinated(b.agents.size());  // Stores which agents have been vaccinated this round
     unsigned int collisions = 0;
 
     #pragma omp parallel for
     for (int i = 0; i < n_vaccinations; i++) {
-        int n = dis(gen);
+        uint64_t n = dis(gen);
         // Finds the first agent with cumulative weight greater than n
         auto it = std::lower_bound(cumulative_weights.begin(), cumulative_weights.end(), n);
         unsigned int agent = it - cumulative_weights.begin();
@@ -451,7 +465,7 @@ void vaccinate(board& b, unsigned int n_vaccinations, std::mt19937_64& gen) {
             b.vaccination_weight_sum.fetch_sub(b.vaccination_weights[agent], std::memory_order_acq_rel);
             b.vaccination_weights[agent] = 0;
         } else {
-            // If it's been vaccinated, we will need to do it over
+            // If it's already been vaccinated, we will need to do this vaccination over
             collisions++;
         }
     }
@@ -486,7 +500,6 @@ int main() {
 
     for (unsigned int t = 0; t < MAX_TIME; t++)
     { //Loop tracking
-        std::cout << "kom hit: ";
 
         visualization_of_board(uppsala_curr);
 
