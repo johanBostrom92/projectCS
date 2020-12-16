fc0a5715e76438e350b5861c9128a06ddf871efe
Hampus Adolfsson <hampus@adolfsson.email>
2020-12-16 15:05:09 +0100

    Add beta value to agent types, make radius just a single value

--- src/parameters.hh
+++ src/parameters.hh
@@ -16,7 +16,7 @@
 #define LAMBDA 2.5  // How fast the INFECTION_RADIUS decrease in case of a lock down
 #define ONLY_ELIGIBLE false // If TRUE, it chooses only an eligible target to infect. If FALSE, any target can be chosen/tried.
 #define PLOT true   // if we want to do plots (increases execution time drastically)
-#define SCALE 1 //scale for geobubble plot. 
+#define SCALE 1 //scale for geobubble plot.
 
 #define VACCINATION_RATE 28 //The time it takes for the vaccine to work
 #define VACCINATION_EFFICACY 75 //The efficiency of the vaccine
@@ -33,14 +33,14 @@ constexpr vaccination_strategy VACC_STRAT = vaccination_strategy::LOW_DENSITY;
 
 
 /**
- * Defines a type of agent, by its infection radius and how often it should occur.
+ * Defines a type of agent, by its infection radius, infection probability and how often it should occur.
  * The infection radius is defined by a normal distribution.
  */
 struct agent_type {
-    /** The mean of the infection radius */
-    float radius_mean;
-    /** The standard deviation of the infection radius. Can be set to 0 to have all agents take radius_mean */
-    float radius_stddev;
+    /** How far away agents may interact with other agents */
+    float infection_radius;
+    /** The probability of an infected agent infecting another agent it interacts with */
+    unsigned int infection_probability;
     /** The weight of the type, determining its relative frequency. A type with weight 2 will be twice as common as one with weight 1, etc. */
     double weight;
 };
@@ -49,7 +49,5 @@ struct agent_type {
  * Gives all agent types to use for the simulation
  */
 static const std::vector<agent_type> AGENT_TYPES = {
-    agent_type{ INFECTION_RADIUS, 0, 1 }
+    agent_type{ INFECTION_RADIUS, INFECTION_PROBABILITY, 1 }
 };
-
-//static_assert(DIM >= INFECTION_RADIUS, "The infection radius cannot be greater than the board dimension");
